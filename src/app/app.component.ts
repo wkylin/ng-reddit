04ffbd02fa17076858c5e180632b647ac1cb0aba
wkylin.w <wkylin.w@gmail.com>
2017-05-13 11:54:46 +0800

    module article

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import {Component} from '@angular/core';
-
+import {ArticleModule} from './article/article.module'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // title = 'app works!';
+  articles: ArticleModule[];
+  
+  constructor(){
+    this.articles=[
+      new ArticleModule('Angular 2', 'http://angular.io', 10),
+      new ArticleModule('Fullstack', 'http://fullstack.io', 12),
+      new ArticleModule('Angular Homepage', 'http://angular.io', 16)
+    ]
+  }
+  
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new ArticleModule(title.value, link.value, 0));
+    title.value='';
+    link.value='';
     return false;
+  }
+  
+  sortedArticles(): ArticleModule[]{
+    return this.articles.sort((a: ArticleModule, b: ArticleModule)=>b.votes - a.votes);
   }
 }
--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,17 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ArticleModule} from './article.module';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css'],
-  host: {
-    class: 'row'
-  }
+  styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
-  article: ArticleModule;
+  @Input() article: ArticleModule;
+  
   constructor() {
-    this.article = new ArticleModule('Angular 2', 'http://angular.io', 10);
+    // this.article = new ArticleModule('Angular 2', 'http://angular.io', 10);
   }
   voteUp(): boolean {
     this.article.voteUp();
